f7eea62e7dd1c547765ce39d370e872c2be36c81
Gabriel Neves <gabriel.neves@zrp.com.br>
2022-06-13 20:33:32 -0300

    first commit

---
+++ bezkoder-app/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+require("dotenv").config(); // inicia lib que pega variaveis de ambiente
+// const bodyParser = require("body-parser"); /* deprecated */
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081" // origem de onde esta partindo a requisicao do front. pode ser '*'
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());  /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+
+const db = require("./app/models");
+
+db.sequelize.sync();
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
